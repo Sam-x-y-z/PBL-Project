66762b78df5883911ca1f449ec80f181e608bb9c
Abdulrahman Elsayed Abdelazeam Elsayed Badawy <69880353+MrThetaIII@users.noreply.github.com>
2024-05-23 00:39:31 +0300

    Final + Demo

--- Training LSTM-MLP/LossFunction.py
+++ Training LSTM-MLP/LossFunction.py
@@ -2,9 +2,10 @@ import torch
 from torch import nn
  
 class CustomLoss(nn.Module):
-    def __init__(self, lambda_value = 3):
+    def __init__(self, lambda_value = 1.5, mu_value = 1.5):
         super(CustomLoss, self).__init__()
         self.lambda_value = lambda_value
+        self.mu_value = mu_value
 
     def forward(self, predicted, true):
         n = predicted.size(0)  # Number of samples
@@ -21,22 +22,7 @@ class CustomLoss(nn.Module):
         
         correlation_term = 2 / ((n * sum_xy - sum_x * sum_y) / ((n*sum_x_sqr-sum_x**2)*(n*sum_y_sqr-sum_y**2))**0.5 + 1) - 1
         
-        term_1 = torch.sum(((predicted >= true) & (true >= 2.5)).float() * torch.log(1 - (predicted - true) / 5))
-        term_2 = torch.sum(((predicted < true) & (true < 2.5)).float() * torch.log(1 - (true - predicted) / 5))
+        error_term = (-torch.sum(torch.log(1 - torch.abs(predicted - true) / 5))) / n
         
-        term_3 = torch.sum(((predicted >= true) & (true < 2.5)).float() * torch.log(1 - (predicted - true) / 5))
-        term_4 = torch.sum(((predicted < true) & (true >= 2.5)).float() * torch.log(1 - (true - predicted) / 5))
-        
-        if torch.isnan(term_1):
-            term_1 = torch.tensor(0, requires_grad=True, dtype=torch.float32)
-        if torch.isnan(term_2):
-            term_2 = torch.tensor(0, requires_grad=True, dtype=torch.float32)
-        if torch.isnan(term_3):
-            term_3 = torch.tensor(0, requires_grad=True, dtype=torch.float32)
-        if torch.isnan(term_4):
-            term_4 = torch.tensor(0, requires_grad=True, dtype=torch.float32)
-        
-        error_term = (- (term_1 + term_2) - self.lambda_value * (term_3 + term_4)) / n
-        
-        loss =  error_term + 1.5 * deviation_term + 1.5 * correlation_term ** 2
+        loss =  error_term + self.lambda_value * deviation_term + self.mu_value * correlation_term ** 2
         return loss
